7ae370c719d61e8d0796adfef34f9b1da8a9e917
NebsNebbier <NebsNebbier@gmail.com>
2013-04-26 09:38:09 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3MongoidDevise::Application.config.secret_token = '84c1bbdd66e4a1e6ba8bcf7dbbcbc317c6262df8b020b22ce7d5d66c2a62372b5deeb7a6d3305f92a42d7544bd7c8e05e0d36873cdf9776509d8fbe869213e0b'
